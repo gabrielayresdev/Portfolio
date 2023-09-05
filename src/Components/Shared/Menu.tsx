8771131c1e3c71a6bf52b406b9cd0e2049101950
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-09-05 16:06:17 -0300

    Feat: Header do site

---
+++ src/Components/Shared/Menu.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Menu.module.sass";
+import burguer from "../../assets/bars-solid.svg";
+import logo from "../../assets/code-solid.svg";
+
+const Menu = () => {
+  const [active, setActive] = React.useState(false);
+
+  const closeModal: React.MouseEventHandler = ({ target, currentTarget }) => {
+    if (currentTarget === target) setActive(false);
+  };
+
+  return (
+    <header className={styles.menu}>
+      <h1>
+        <img src={logo} alt="" />
+        Gabriel Ayres
+      </h1>
+      <nav className={active ? styles.active : ""}>
+        <img
+          src={burguer}
+          alt=""
+          className={styles.menuBtn}
+          onClick={() => {
+            setActive((current) => !current);
+          }}
+        />
+        <div className={styles.ulContainer} onClick={closeModal}>
+          <ul>
+            {["sobre", "projetos", "certificados", "contato"].map((value) => (
+              <li key={value}>
+                <a onClick={() => setActive(false)}>{value}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Menu;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from "../Components/Shared/Menu";
+
+const Home = () => {
+  return (
+    <>
+      <Menu />
+    </>
+  );
+};
+
+export default Home;
